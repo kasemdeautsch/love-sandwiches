f2e10f13f0a4ddcfc4993ffd2b9c1a13e30c72f8
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-12-17 13:37:52 +0000

    bring last stock data

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -47,7 +48,8 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
-        [int(value) for value in values]
+        x = [int(value) for value in values]
+
         if len(values) != 6:
             raise ValueError(
                 f"--Exactly 6 values required, you provided {len(values)}--"
@@ -55,8 +57,34 @@ def validate_data(values):
     except ValueError as e:
         print(f"XXX.Invalid data: {e}, please try again.XXX\n")
         return False
-
+    print('x ', x)
+    print('values ', values)
     return True
 
 
-data = get_sales_data()
+def update_sales_work_sheet(data):
+    print('updating sales in work sheet')
+    sales_work_sheet = SHEET.worksheet('sales')
+    sales_work_sheet.append_row(data)
+    print('sales updated.\n')
+
+
+def calculate_surplus_data(sales_row):
+    print('calculate surplus starting')
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    pprint(stock_row)
+
+
+def main():
+    """Run all program functions"""
+    data = get_sales_data()
+    print('data ', data)
+    sales_data = [int(num) for num in data]
+    print(sales_data)
+    update_sales_work_sheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print('Welcom to lovesand project..')
+main()
